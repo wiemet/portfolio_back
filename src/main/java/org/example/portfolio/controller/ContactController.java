270f6a0a058989c61b2096871a53a1e230626c27
Wiem Ben Romdhane <wiembenromdhane129@gmail.com>
2026-04-23 20:04:53 +0100

    fix CORS production

--- src/main/java/org/example/portfolio/controller/ContactController.java
+++ src/main/java/org/example/portfolio/controller/ContactController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ContactController {
     @Autowired
     private ContactService contactService;
